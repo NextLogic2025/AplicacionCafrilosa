92f817037ada8ac5725f3c8c663ec71d4733e8bb
Denis Ruiz <denisruiz112789@gmail.com>
2026-01-16 14:08:56 -0500

    Test

--- aplicacion/frontend/mobile/babel.config.js
+++ aplicacion/frontend/mobile/babel.config.js
@@ -1,7 +1,8 @@
 module.exports = function (api) {
   api.cache(true)
+  const isTest = process.env.NODE_ENV === 'test' || process.env.BABEL_ENV === 'test'
   return {
-    presets: ['babel-preset-expo', 'nativewind/babel'],
+    presets: ['babel-preset-expo', ...(isTest ? [] : ['nativewind/babel'])],
     plugins: ['react-native-reanimated/plugin'],
   }
 }
